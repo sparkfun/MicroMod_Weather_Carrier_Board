6cb3a2e3061f032da775a34de43da230df218212
pmakin-png <54557724+pmakin-png@users.noreply.github.com>
2020-07-01 08:33:17 -0600

    Update MM_Weather_CB_Test.ino

--- MM_Weather_CB_Test/MM_Weather_CB_Test.ino
+++ MM_Weather_CB_Test/MM_Weather_CB_Test.ino
@@ -6,19 +6,51 @@
   DONE: BME280 - temp, pressure, humidity (I2C)
   DONE: VEML6075 - UV sensor (I2C)
   AS3935 - lightning detector (SPI)
-  Soil moisture connector?
-  Rain and Wind meters?
-  uSD card?
+  DONE: Soil moisture connector
+  DONE: Wind Meter
+  Rain Meter
+  DONE: uSD card
+  Other
+    PWM0/1 -- 1 doesn't work
+    DONE: Qwiic Connector
+    TX/RX 1/2 -- tested in separate sketch, need diff PB to test TX/RX2
+    VIN/3
 */
 
 #include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
 #include "SparkFunBME280.h"
 #include <SparkFun_VEML6075_Arduino_Library.h>
+#include <SparkFun_Alphanumeric_Display.h>  
 
 BME280 tempSensor;
 VEML6075 uv;
+Sd2Card card;
+SdVolume volume;
+HT16K33 display;  //For testing the qwiic connector
 
 int STAT_LED = 19;
+int moist_val = 0;  //Variable for storing moisture value
+int soilPin = A32;  //Pin number that measures analog moisture signal
+int soilPower = A16;  //Pin number that will power the soil moisture sensor
+int WSPEED = 1; //Digital I/O pin for wind speed
+int WDIR = A35; //Analog pin for wind direction
+int RAIN = 2;   //Digital I/O pin for rain fall
+const int SD_chipSelect = 23;
+//const int SD_chipSelect = CS; //Chip select pin for SD card
+
+//Function is called every time the rain bucket tips
+void rainIRQ()
+{
+  Serial.println("RAIN!");
+}
+
+//Function is called when the magnet in the anemometer is activated
+void wspeedIRQ()
+{
+  Serial.println("Wind click!");
+}
 
 void setup() {
   Serial.begin(115200);
@@ -37,10 +69,25 @@ void setup() {
   }
   
   pinMode(STAT_LED, OUTPUT);
+  pinMode(soilPower, OUTPUT);
+  digitalWrite(soilPower, LOW);
+
+  //Initialization for weather meter
+  pinMode(WSPEED, INPUT_PULLUP);  //Input from wind meters windspeed sensor
+  pinMode(RAIN, INPUT_PULLUP);    //Input from wind meters rain gauge sensor
+  //attach external interrupt pins to IRQ functions
+  attachInterrupt(0, rainIRQ, FALLING);
+  attachInterrupt(1, wspeedIRQ, FALLING);
+  //turn on interrupts
+  interrupts();
+
+  testSD();
+  testRemainingPeripherals();
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  Serial.println();
   Serial.println("BME280 Check");
   Serial.print("Temperature: ");
   Serial.println(tempSensor.readTempF(), 2);
@@ -55,9 +102,129 @@ void loop() {
   Serial.println("VEML6075 Check");
   Serial.println(String(uv.uva()) + ", " + String(uv.uvb()) + ", "+ String(uv.index()));
   Serial.println();
- 
+
+  Serial.println("Soil Moisture Sensor Check");
+  Serial.print("Soil Moisture = ");
+  Serial.println(readSoil());
+
+  Serial.println("Weather Meter Wind Direction Check");
+  Serial.print("Wind direction: ");
+  Serial.print(get_wind_direction());
+  Serial.println(" degrees");
+  
   digitalWrite(STAT_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(STAT_LED, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+
+  delay(5000);
+}
+
+void testSD() {
+  //SD Card test
+  Serial.println("Initializing SD card...");
+  
+  if (!card.init(SPI_HALF_SPEED, SD_chipSelect)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correc?");
+    Serial.println("* did you change the chipSelect pin to match your module?");
+    while(1);
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+
+  //print the type of card
+  Serial.println();
+  Serial.print("Card type:          ");
+  switch (card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition. \nMake sure you've formatted the card");
+    while(1);
+  }
+  
+  //print the type and size of hte first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("Volume type is:     FAT");
+  Serial.println(volume.fatType(), DEC);
+
+  volumesize = volume.blocksPerCluster();
+  volumesize *= volume.clusterCount();
+  volumesize /= 2;
+  volumesize /= 1024;
+  Serial.print("Volume size (Gb): ");
+  Serial.println((float)volumesize / 1024.0);
+}
+
+int readSoil() {
+  //Power Senor
+  digitalWrite(soilPower, HIGH);
+  delay(10);
+  moist_val = analogRead(soilPin);  //Read the SIG value from sensor
+  digitalWrite(soilPower, LOW); //Turn the sensor off
+  return moist_val; //Return current moisture value
+}
+
+int get_wind_direction()
+{
+  unsigned int adc;
+  adc = analogRead(WDIR); //get the current readings from the sensor
+
+  if (adc < 380) return (113);
+  if (adc < 393) return (68);
+  if (adc < 414) return (90);
+  if (adc < 456) return (158);
+  if (adc < 508) return (135);
+  if (adc < 551) return (203);
+  if (adc < 615) return (180);
+  if (adc < 680) return (23);
+  if (adc < 746) return (45);
+  if (adc < 801) return (248);
+  if (adc < 833) return (225);
+  if (adc < 878) return (338);
+  if (adc < 913) return (0);
+  if (adc < 940) return (293);
+  if (adc < 967) return (315);
+  if (adc < 990) return (270);
+  return (-1);
+}
+
+void testRemainingPeripherals() {
+  Serial.println();
+  //Test Qwiic Connector
+  Serial.println("Test Qwiic Connector");
+  if (display.begin() == false)
+  {
+    Serial.println("Alphanumeric did not acknowledge! Freezing.");
+    while(1);
+  }
+  Serial.println("Alphanumeric acknowledged.");
+  display.print("Milk");
+  Serial.println();
+  
+  //Test PWM0/1
+  Serial.println("Test PWM0/1. Use multimeter to verify.");
+  analogWrite(44, 125);
+//  analogWrite(45, 125); //TODO: this one doesn't work
+  Serial.println();
+
+  //Test VIN/3
+  Serial.println("VIN/3 Test");
+  analogReadResolution(14);
+  int temp = analogRead(A31);
+//  temp = (temp * 3 * 2)/16384;
+  Serial.println(temp);
 }
